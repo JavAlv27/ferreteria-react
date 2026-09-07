57637e0c3a950fd8bb1eb6af8ae30e403acd5190
Javier Alvarez <javier.alvarez3@virginiogomez.cl>
2026-09-07 03:43:35 -0300

    style: diseno profesional, moderno y responsivo con styles.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { FormApp } from './formApp'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
